be2d649c127839f44f5ce08aa1dfee179e380aca
赵坤 <zk9474@hotmail.com>
2024-09-20 17:23:48 +0800

    answerText

--- packages/plugins/src/sleep/index.ts
+++ packages/plugins/src/sleep/index.ts
@@ -1,14 +1,21 @@
+import { NodeInputKeyEnum } from '@fastgpt/global/core/workflow/constants';
+
 type Props = {
   milliseconds: number;
 };
 
 //  模拟休眠函数
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+type Response = Promise<{
+  [NodeInputKeyEnum.answerText]: string;
+}>;
 
-const main = async ({ milliseconds }: Props): Promise<void> => {
+const main = async ({ milliseconds }: Props): Response => {
   //  执行休眠
   await sleep(milliseconds);
-  return;
+  return {
+    [NodeInputKeyEnum.answerText]: ''
+  };
 };
 
 export default main;
